13d50449b526984f2b05a0c3c7ca91018ed566d3
Austin Hooker <ahooker@elevenfifty.org>
2021-10-29 10:14:44 -0400

    controller_methods_still_need_update

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantRaterAPI.Models;
 
 namespace RestaurantRaterAPI.Controllers
 {
@@ -10,6 +13,47 @@ namespace RestaurantRaterAPI.Controllers
         public RestaurantController(RestaurantDbContext context)
         {
             _context = context;
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostRestaurant([FromForm] RestaurantEdit model)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+            _context.Restaurants.Add(
+            new Restaurant
+            {
+                Name = model.Name,
+                Location = model.Location
+
+            });
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRestaurants()
+        {
+            var restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetRestaurantById(int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if(restaurant == null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteRestaurantById([FromRoute] int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            var removeRest = _context.Restaurants.Remove(restaurant);
+            await _context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
